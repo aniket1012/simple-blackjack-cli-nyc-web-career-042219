b950dd6ef8bf448a62888bbdedcd2e841eeedfd2
Aniket Patel <aniketupatel1@gmail.com>
2019-04-16 21:25:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,8 +30,18 @@ def initial_round
   return card_total
 end
 
-def hit?
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input
 
+  if user_input == 'h'
+    card_total += deal_card
+
+  elsif user_input == 's'
+    card_total
+  else
+
+  end
 end
 
 def invalid_command
